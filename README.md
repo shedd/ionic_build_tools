7001baa6a9c645a0ed02934cdd5e162584f5571e
Robert Shedd <robert@shedd.us>
2014-08-04 23:57:40 -0700

    lots of improvements to the build tooling based on ongoing development

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A template app using the Ionic Framework and AngularJS to demonstrate a set of Gulp build tools.
 
+Before using, check the TODOs marked in the `gulpfile`.
+
 ## Repo Structure
 
 A brief tour of the repository:
@@ -17,12 +19,60 @@ A brief tour of the repository:
 
 ## Installing Dependencies
 
-Install the packages required:
+Install the Node packages required:
 
     sudo npm install -g ionic
     sudo npm install -g cordova
+    
+    # if you want to run the iOS emulator, install the following dependency
+    # NOTE: THIS REQUIRES XCode 5.1.1 WHICH REQUIRES OS X MAVERICKS
+    # if you're not running the iOS simulator, you can skip this dependency
     sudo npm install -g ios-sim
+
     npm install
+
+    # we also need a couple of local versions, so we have it available for the hooks/after_prepare
+    npm install cordova
+    npm install cordova-lib
+    npm install gulp # we needed to run this to get the node binary installed
+
+Install ImageMagick for icon processing:
+
+    brew update
+    brew install imagemagick --build-from-source
+
+(We need to use the build from source option in Mac OS X Mavericks to get PNG support.  If that fails, see https://github.com/Homebrew/homebrew/issues/14325#issuecomment-45429003 )
+
+Check to make sure that NPM modules are added to your path.  If not, and you installed Node via Homebrew, add this to your .zshrc/.bashrc:
+
+    # add node_modules to path
+    export PATH=$PATH:./node_modules/.bin
+
+### Android Dependencies
+
+* Install the Android SDK - download from: http://developer.android.com/sdk/index.html
+* Extract the SDK and move the folder into `~/Development`
+* Create a symlink to the extracted version:
+
+        cd ~/Development
+        ln -s adt-bundle-mac-x86_64-20140321 adt-bundle
+
+* Add the SDK to your path - edit `.bash_profile` or `.zshrc` or similar:
+
+        # add android dev tools to path
+        export PATH=$PATH:~/Development/adt-bundle/sdk/platform-tools:~/Development/adt-bundle/sdk/tools
+
+* Ensure Java is installed
+* Install Ant through Homebrew:
+
+        brew update
+        brew install ant
+
+* Create an emulator image:
+
+        android create avd --name kitkat --target android-19
+
+For help, see the Cordova Android Platform Guide: http://cordova.apache.org/docs/en/3.4.0/guide_platforms_android_index.md.html
 
 ## Building & Testing
 
@@ -40,13 +90,19 @@ The primary configuration the build target controls is the API hostname:
 
 ### Local Browser Testing
 
-Run the following command to build the application code, start the watch task, and load it on the local Ionic testing server with LiveReload enabled:
+Run the default gulp task to build the application code, start the watch task, and load it on the local Ionic testing server with LiveReload enabled:
 
-    BUILD_TARGET=development gulp serve 
+    BUILD_TARGET=development gulp
 
 See more about testing here: http://ionicframework.com/docs/guide/testing.html
 
 ### Running Development Builds on Emulators
+
+First, do a recompile:
+
+    BUILD_TARGET=development gulp compile
+
+**This will be combined back into the `build` command, but struggling with async/sync gulp tasks**
 
 Kick-off the build for the platform you want to test:
 
@@ -60,18 +116,168 @@ If you want to run the build on the platform's emulator, add the `--run` option:
 
 ### Release Builds
 
-This is TODO.
+This section is still in progress.
 
 See more about publishing here: http://ionicframework.com/docs/guide/publishing.html
 
+#### iOS
+
+See http://docs.phonegap.com/en/edge/guide_platforms_ios_index.md.html
+
+##### Compile the code
+
+Compile the code & build for iOS, configured for the environment desired - do this before opening XCode:
+
+For staging:
+
+    gulp patch
+    BUILD_TARGET=staging gulp compile --release
+    BUILD_TARGET=staging gulp build --ios --release
+
+For production:
+
+    gulp patch
+    BUILD_TARGET=production gulp compile --release
+    BUILD_TARGET=production gulp build --ios --release
+
+##### Setup iOS Device for Development
+
+* Open the XCode project that Cordova generates
+* Plug in your test device
+* In the XCode Organizer window, click Use for Development
+* XCode will prompt you to add the device to an iOS Developer account
+* XCode will prompt you to generate a certificate
+
+##### Deploy to Device
+
+* Use the USB cable to plug the device into your Mac.
+* Select the name of the project in the Xcode window's Scheme drop-down list.
+* Select your device from the Device drop-down list. If it is plugged in via USB but still does not appear, press the Organizer button to resolve any errors.
+* Press the Run button to build, deploy and run the application on your device.
+
+
+#### Android
+
+Follow these instructions to setup your device for testing: http://developer.android.com/tools/device.html
+
+##### Setup Code Signing
+
+Unless you are changing something, this has already been setup for you.
+
+Generate a new key if needed:
+
+    keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+
+With the details from the key you just generated, create an `ant.properties` file in `platforms/android` with the following content (as discussed here: http://stackoverflow.com/questions/20402818/build-android-release-apk-on-phonegap-3-x-cli/22979830#22979830):
+
+    key.store=/Path/to/KeyStore/myapp-release-key.keystore
+    key.alias=myapp
+
+This will mean you'll be prompted for your keystore password during the build process.
+
+##### Configure the keystore password
+
+Create a `secure.properties` file in `platforms/android` to contain the keystore password.
+
+The content of `secure.properties` should be the following (as discussed here http://stackoverflow.com/a/14765511/139934):
+
+    key.store.password=<your keystore password>
+    key.alias.password=<your alias password>
+
+(This file is in the `.gitignore` and will NOT be checked into version control.)
+
+This file is referenced in `platforms/android/custom_rules.xml`
+
+##### Run the build
+
+The release build has been scripted via Gulp to automate build cleaning.
+
+For staging:
+
+    gulp patch
+    BUILD_TARGET=staging gulp compile --release
+    BUILD_TARGET=staging gulp build --android --release
+
+For production:
+
+    gulp patch
+    BUILD_TARGET=production gulp compile --release
+    BUILD_TARGET=production gulp build --android --release
+
+By default, the build output file ends up in the 'platforms/android/ant-build' directory already signed and aligned and ready to deploy.
+
+The Gulp script copies the output apk to `./release_builds/` with the timestamp appended.
+
+You can install this to your device using `adb` as follows:
+
+    adb install -r ./release_builds/release.apk
+
+#### Summarized Production Release Sequence
+
+    gulp patch
+    BUILD_TARGET=production gulp compile --release
+    BUILD_TARGET=production gulp build --ios --release
+    BUILD_TARGET=production gulp compile --release
+    BUILD_TARGET=production gulp build --android --release
+
 
 ## Development
+
+### Bump app versions
+
+Every time a build is release, the app's versions should be bumped in the relevant files.
+
+The following `gulp` commands have been implemented to bump the version.
+
+The app is versioned with semantic versioning - please see http://semver.org
+
+ * MAJOR ("major") version when you make incompatible API changes -- major: 1.0.0
+ * MINOR ("minor") version when you add functionality in a backwards-compatible manner -- minor: 0.1.0
+ * PATCH ("patch") version when you make backwards-compatible bug fixes. -- patch: 0.0.2
+ * PRERELEASE ("prerelease") a pre-release version -- prerelease: 0.0.1-2
+
+You can use the commands
+
+    gulp patch     # makes v0.1.0 → v0.1.1
+    gulp feature   # makes v0.1.1 → v0.2.0
+    gulp release   # makes v0.2.1 → v1.0.0
+    gulp prerelease # makes v0.2.1-2 → v1.0.0-2
+
+### Android ADB Quick Reference
+
+    adb devices     # see Android devices attached to system
+    adb install <file.apk>     # install apk to device
+    adb install -r <file.apk>   # replace existing file.apk while preserving data
+    adb shell screenrecord --time-limit 10 /sdcard/demo.mp4   # take a screencast of device for 10 seconds
+    adb pull /sdcard/demo.mp4       # retrieve file from device
+
+### Resolving error 'Not an Android project'
+
+If when running a Cordova command, you get an error like the following: `The provided path "/Code/.../platforms/android" is not an Android project.`
+
+This is because `AndroidManifest.xml`, which is preprocessed for builds, does not currently exist in `./platforms/android`
+
+Just run `gulp process-android-build-config --android` which will generate another version for you and you should be set.
+
+### Installing New Dependencies
+
+Currently, frontend dependencies (i.e. those loaded by Angular) are managed by Bower via `bower.json`.  Install new dependencies via Bower as follows:
+
+    bower install dependency --save
+
+Build dependencies (i.e. gulp) are managed by NPM via `package.json`.  Install new development dependencies via NPM as follows:
+
+    npm install dependency --save-dev
 
 ### Lint your JavaScript code
 
 JS-Hint is integrated into Gulp.  Run:
 
     gulp lint
+
+### Customizing Ionic with SASS
+
+See this guide on customizing Ionic using SASS: http://ionicframework.com/tutorials/customizing-ionic-with-sass/
 
 ### Updating Ionic
 
@@ -102,3 +308,11 @@ If you feel daring and want use the bleeding edge 'Nightly' version of Ionic, ch
 ```
 
 Warning: the nightly version is not stable.
+
+# Troubleshooting
+
+## XCode: Code Signing Failed: Check that the identity you selected is valid
+
+It seems that XCode has real problems with directory structures in Resources.  Check the System Console because it seems that this is where the errors are spit out.
+
+The one time this happened to me, a `node_modules` directory ended up getting caught in the `www/lib` directory from building a dependency.  Deleting this fixed the issue.  Don't forget to remove this from the platforms copy of the www directory, too.
--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,15 @@ var stylish = require('jshint-stylish');
 var changed = require('gulp-changed');
 var connect = require('gulp-connect');
 var open = require("gulp-open");
+var clean = require('gulp-clean');
+var rename = require("gulp-rename");
+var moment = require("moment");
+var bump = require('gulp-bump');
+var semver = require('semver');
+var git = require('gulp-git');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
+var Q = require('q');
 
 // the source paths
 var source_paths = {
@@ -23,6 +32,7 @@ var source_paths = {
   scripts: ['./app/js/**/*.js'],
   templates: ['./app/templates/**/*.html'],
   images: ['./app/img/**/*.*'],
+  resources: ['./app/res/**/*.png'],
   index_page: ['./app/index.html']
 };
 // the destination paths
@@ -31,17 +41,26 @@ var dest_paths = {
   scripts: './www/js/',
   templates: './www/templates/',
   images: './www/img/',
-  root: './www/'
+  resources: './www/res/',
+  index_page: './www/index.html',
+  root: './www/',
+  release_builds: './release_builds'
 };
 // the options used by gulp-open when booting the test server
 var open_options = {
   url: "http://localhost:8080"
 };
 
-// gulp.task('default', ['sass']);
+gulp.task('default', function(){
+    runSequence('compile', 'serve');
+});
 
-gulp.task('build-styles', function(done) {
-  gulp.src(source_paths.sass)
+///////////////////////////////////////////////////////
+// COMPILATION TASKS
+///////////////////////////////////////////////////////
+
+gulp.task('compile-sass', function() {
+  return gulp.src(source_paths.sass)
     .pipe(changed(dest_paths.css))
     .pipe(sass())
     .pipe(gulp.dest(dest_paths.css))
@@ -50,9 +69,11 @@ gulp.task('build-styles', function(done) {
     }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest(dest_paths.css))
-    .on('end', done)
     .pipe(connect.reload());
-  gulp.src(source_paths.css)
+});
+
+gulp.task('compile-css', function() {
+  return gulp.src(source_paths.css)
     .pipe(changed(dest_paths.css))
     .pipe(gulp.dest(dest_paths.css))
     .pipe(minifyCss({
@@ -60,49 +81,197 @@ gulp.task('build-styles', function(done) {
     }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest(dest_paths.css))
-    .on('end', done)
     .pipe(connect.reload());
 });
 
-gulp.task('build-scripts', function() {
-  gulp.src(source_paths.scripts)
+gulp.task('compile-scripts', function() {
+  return gulp.src(source_paths.scripts)
     .pipe(changed(dest_paths.scripts))
     .pipe(preprocess())
     .pipe(gulp.dest(dest_paths.scripts))
     .pipe(connect.reload());
 });
 
-gulp.task('build-templates', function() {
-  gulp.src(source_paths.templates)
+gulp.task('compile-templates', function() {
+  return gulp.src(source_paths.templates)
     .pipe(changed(dest_paths.templates))
     .pipe(preprocess())
     .pipe(gulp.dest(dest_paths.templates))
     .pipe(connect.reload());
 });
 
-gulp.task('build-index', function() {
-  gulp.src(source_paths.index_page)
+gulp.task('compile-index', function() {
+  return gulp.src(source_paths.index_page)
     .pipe(changed(dest_paths.root))
-    .pipe(preprocess())
+    .pipe(preprocess({context: {RELEASE: argv.release}}))
     .pipe(gulp.dest(dest_paths.root))
     .pipe(connect.reload());
 });
 
-gulp.task('build-images', function() {
-  gulp.src(source_paths.images)
+gulp.task('compile-images', function() {
+  return gulp.src(source_paths.images)
     .pipe(changed(dest_paths.images))
     .pipe(gulp.dest(dest_paths.images))
     .pipe(connect.reload());
 });
 
-gulp.task('build-native', function() {
+// compile resources (icons)
+gulp.task('compile-resources', function() {
+  exec('cd ./app/res/ && sh convert.sh');
+
+  return gulp.src(source_paths.resources)
+     .pipe(changed(dest_paths.resources))
+     .pipe(gulp.dest(dest_paths.resources));
+});
+
+gulp.task('compile-all', ['compile-scripts',
+                          'compile-sass',
+                          'compile-css',
+                          'compile-templates',
+                          'compile-index',
+                          'compile-images',
+                          'compile-resources'
+                          ]);
+
+// compile everything after cleaning the build
+gulp.task('compile', ['build-clean'], function(){
+  var deferred = Q.defer();
+
+  runSequence('compile-all', function(){
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+});
+
+///////////////////////////////////////////////////////
+// CLEAN TASKS
+///////////////////////////////////////////////////////
+
+gulp.task('clean-styles', function() {
+  return gulp.src(dest_paths.css, {read: false})
+         .pipe(clean({force: true}));
+});
+
+gulp.task('clean-images', function() {
+  return gulp.src(dest_paths.images, {read: false})
+       .pipe(clean({force: true}));
+});
+
+gulp.task('clean-scripts', function() {
+  return gulp.src(dest_paths.scripts, {read: false})
+     .pipe(clean({force: true}));
+});
+
+gulp.task('clean-templates', function() {
+  return gulp.src(dest_paths.templates, {read: false})
+       .pipe(clean({force: true}));
+});
+
+gulp.task('clean-resources', function() {
+  return gulp.src(dest_paths.resources, {read: false})
+       .pipe(clean({force: true}));
+});
+
+gulp.task('clean-index', function() {
+  return gulp.src(dest_paths.index_page, {read: false})
+       .pipe(clean({force: true}));
+});
+
+gulp.task('build-clean', ['clean-scripts',
+                          'clean-styles',
+                          'clean-templates',
+                          'clean-images',
+                          'clean-resources'
+                         ]);
+
+///////////////////////////////////////////////////////
+// WATCH TASKS
+///////////////////////////////////////////////////////
+
+gulp.task('watch-sass', function(){
+  return gulp.watch(source_paths.sass, ['compile-sass']);
+});
+
+gulp.task('watch-css', function(){
+  return gulp.watch(source_paths.css, ['compile-css']);
+});
+
+gulp.task('watch-scripts', function(){
+  return gulp.watch(source_paths.scripts, ['compile-scripts']);
+});
+
+gulp.task('watch-templates', function(){
+  return gulp.watch(source_paths.templates, ['compile-templates']);
+});
+
+gulp.task('watch-index_page', function(){
+  return gulp.watch(source_paths.index_page, ['compile-index']);
+});
+
+gulp.task('watch-images', function(){
+  return gulp.watch(source_paths.images, ['compile-images']);
+});
+
+gulp.task('watch', function() {
+  runSequence([ 'watch-sass',
+                'watch-css',
+                'watch-scripts',
+                'watch-templates',
+                'watch-index_page',
+                'watch-images'
+              ]);
+});
+
+///////////////////////////////////////////////////////
+// LOCAL SERVER TASKS
+///////////////////////////////////////////////////////
+
+// start a livereload-enable server after compiling
+// TODO: add 'compile' back as a dependency when sync/async issues fixed
+gulp.task('run-server', [], function() {
+  return connect.server({
+    root: 'www',
+    livereload: true
+  });
+});
+
+// open browser after starting server
+gulp.task('open-browser', ['run-server'], function() {
+  return gulp.src(dest_paths.root + "index.html")
+  .pipe(open("", open_options));
+});
+
+// compile then boot up the ionic site in a browser
+gulp.task('serve', ['open-browser', 'watch']);
+
+///////////////////////////////////////////////////////
+// BUILD TASKS
+///////////////////////////////////////////////////////
+
+// set the debuggable flag in the AndroidManifest.xml for release or debug
+// move the manifest file into the build path
+gulp.task('process-android-build-config', function() {
+  if(!argv.android) {
+    return false;
+  }
+
+  var srcManifestFile = ['./lib/AndroidManifest.xml'];
+  var destManifestFile = './platforms/android/';
+  return gulp.src(srcManifestFile)
+    .pipe(preprocess({context: {RELEASE: argv.release}}))
+    .pipe(gulp.dest(destManifestFile));
+});
+
+// build a debug native version after compiling
+// TODO: add 'compile' back as a dependency when sync/async issues fixed
+gulp.task('build-debug', ['process-android-build-config'], function() {
   // do the ionic ios build
   if(argv.ios) {
     if (exec('ionic build ios').code !== 0) {
       echo('Error: iOS build failed');
       exit(1);
     }
-
   }
   // do the ionic android build
   if(argv.android) {
@@ -113,15 +282,64 @@ gulp.task('build-native', function() {
   }
 });
 
+// build a release native version after compiling
+// TODO: add 'compile' back as a dependency when sync/async issues fixed
+gulp.task('build-release', ['process-android-build-config'], function() {
+  // remove the console plugin
+  exec("cordova plugin rm org.apache.cordova.console");
+
+  // do the ionic ios build
+  if(argv.ios) {
+    if (exec('cordova build --release ios').code !== 0) {
+      echo('Error: iOS build failed');
+      exit(1);
+    }
+  }
+
+  // do the ionic android build
+  if(argv.android) {
+    // clean the android build folders:
+    gulp.src('./platforms/android/ant-build/', {read: false})
+      .pipe(clean({force: true}));
+    gulp.src('./platforms/android/ant-gen/', {read: false})
+      .pipe(clean({force: true}));
+
+    if (exec('cordova build --release android').code !== 0) {
+      echo('Error: Android build failed');
+      exit(1);
+    }
+    else {
+      // copy the release output to release-builds/
+      // TODO: change the below to match your expected release filename
+      return gulp.src(['./platforms/android/ant-build/release.apk'])
+              .pipe(rename(function (path) {
+                  // see https://github.com/hparra/gulp-rename for info on rename fields
+                  path.basename += moment().format('MMDDYYYY-hhmmss');
+              }))
+              // do any other processing needed
+              .pipe(gulp.dest(dest_paths.release_builds));
+    }
+  }
+
+  // re-add the console plugin
+  // exec("cordova plugin add org.apache.cordova.console");
+});
+
+// fire up the emulator, depending on what flags passed
 gulp.task('run-emulator', function() {
+  // should we trigger the emulator?
+  if(!argv.run) {
+    return false;
+  }
+
   // start the ios emulator
   if(argv.ios) {
     if (exec('ionic emulate ios').code !== 0) {
       echo('Error: iOS run failed');
       exit(1);
     }
-
   }
+
   // start the android emulator
   if(argv.android) {
     if (exec('ionic emulate android').code !== 0) {
@@ -131,76 +349,96 @@ gulp.task('run-emulator', function() {
   }
 });
 
-// run all of the individual build processes
-gulp.task('compile-all', function(callback) {
-  runSequence(['build-scripts', 
-               'build-styles',
-               'build-templates',
-               'build-index',
-               'build-images'
-              ],
-               callback
-            );
-});
-
-// start a livereload-enable server & open browser
-gulp.task('run-server', function() {
-  connect.server({
-    root: 'www',
-    livereload: true
-  });
-  gulp.src(dest_paths.root + "index.html")
-  .pipe(open("", open_options));
-});
-
-gulp.task('watch-styles', function(){
-  return gulp.watch(source_paths.sass, ['build-styles']);
-});
-
-gulp.task('watch-scripts', function(){
-  return gulp.watch(source_paths.scripts, ['build-scripts']);
-});
-
-gulp.task('watch-templates', function(){
-  return gulp.watch(source_paths.templates, ['build-templates']);
-});
-
-gulp.task('watch-index_page', function(){
-  return gulp.watch(source_paths.index_page, ['build-index']);
-});
-
-gulp.task('watch-images', function(){
-  return gulp.watch(source_paths.images, ['build-images']);
-});
-
-gulp.task('watch', function() {
-  return runSequence([  'watch-styles',
-                        'watch-scripts',
-                        'watch-templates',
-                        'watch-index_page',
-                        'watch-images'
-                      ]);
-});
-
-// compile then boot up the ionic livereload server
-gulp.task('serve', function() {
-  runSequence('compile-all', ['watch', 'run-server']);
-});
-
 gulp.task('build', function() {
-  runSequence('compile-all', 'build-native');
-  
-  // should we trigger the emulator?
-  if(argv.run) {
-    runSequence('run-emulator');
+  // are we building a debug or release version?
+  if(argv.release) {
+    runSequence('build-release');
+  }
+  else {
+    runSequence('build-debug', 'run-emulator');
   }
 });
 
-gulp.task('build-release', function() {
-  echo('Error: this is unimplemented');
-  exit(1);
-});
+///////////////////////////////////////////////////////
+// MISC TASKS
+///////////////////////////////////////////////////////
 
+// used for bumping versions and getting the version info
+// `fs` is used instead of require to prevent caching in watch (require caches)
+var fs = require('fs');
+var getPackageJson = function () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
+
+/**
+ * Bumping version number and tagging the repository with it.
+ *
+ * Semantic versioning bump
+ * Please read http://semver.org/
+ * **************************************************
+ * MAJOR ("major") version when you make incompatible API changes -- major: 1.0.0
+ * MINOR ("minor") version when you add functionality in a backwards-compatible manner -- minor: 0.1.0
+ * PATCH ("patch") version when you make backwards-compatible bug fixes. -- patch: 0.0.2
+ * PRERELEASE ("prerelease") a pre-release version -- prerelease: 0.0.1-2
+ * **************************************************
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *     gulp prerelease # makes v0.2.1-2 → v1.0.0-2
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+function inc(importance) {
+  var deferred = Q.defer();
+  // reget package
+  var pkg = getPackageJson();
+  // get existing version
+  var oldVer = pkg.version;
+  // increment version
+  var newVer = semver.inc(oldVer, importance);
+  // json filter
+  var jsonFilter = filter('**/*.json');
+
+  // TODO: change /platforms/ios/project/ in the below to match your Cordova project name
+
+  // bump the version number in the xml config files
+  replacement = 'sed -i \'\' -e \'s/version=\"'+ oldVer + '\"/version=\"' + newVer + '\"/\' ./config.xml'
+  exec(replacement);
+  replacement2 = 'sed -i \'\' -e \'s/version=\"'+ oldVer + '\"/version=\"' + newVer + '\"/\' ./platforms/ios/project/config.xml'
+  exec(replacement2);
+  replacement3 = 'sed -i \'\' -e \'s/android:versionName=\"'+ oldVer + '\"/android:versionName=\"' + newVer + '\"/\' ./lib/AndroidManifest.xml'
+  exec(replacement3);
+
+  // get all the files to bump version in
+  gulp.src(['./package.json', './bower.json', './config.xml', './platforms/ios/project/config.xml', './lib/AndroidManifest.xml'])
+      // filter only the json files
+      .pipe(jsonFilter)
+      // bump the version number in the json files
+      .pipe(bump({version: newVer}))
+      // save json files back to filesystem
+      .pipe(gulp.dest('./'))
+      // restore full stream
+      .pipe(jsonFilter.restore())
+      // commit the changed version number
+      .pipe(git.commit('bump app version to ' + newVer))
+      // read only one file to get the version number
+      .pipe(filter('package.json'))
+      // **tag it in the repository**
+      .pipe(tag_version());
+
+  return deferred.promise;
+}
+
+gulp.task('patch', function() { return inc('patch'); })
+gulp.task('feature', function() { return inc('minor'); })
+gulp.task('release', function() { return inc('major'); })
+gulp.task('prerelease', function() { return inc('prerelease'); })
+
+// run source scripts through JSHint
 gulp.task('lint', function() {
   return gulp.src(source_paths.scripts)
     .pipe(jshint())
